571e136500a2236db95a1ab1ef7856bce7c5f246
yannmarc <chamackstone@gmail.com>
2023-08-23 06:57:57 +0100

    "commiting all changes"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <Fragment>
         <footer>
             <div className="main-container lg:max-w-[1100px] lg:mx-auto px-6 xl:px-0 flex flex-col lg:flex-row lg:flex xl:flex-col xl:items-start lg:items-center gap-x-8 py-16 gap-y-8">
-                <div className="flex lg:items-start justify-between w-full">
+                <div className="flex flex-col lg:flex-row gap-y-[80px] lg:gap-y-0 lg:items-start justify-between w-full">
                     <div className="">
                         <div className=""><img src={logo} alt="travel page logo" /></div>
                         <div className="w-full my-6 lg:w-[300px] text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia!</div>
@@ -15,7 +15,7 @@ const Footer = () => {
                             <span className='block'><img src={iconTwitter} alt="twitter icon" /></span>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-[80px]">
+                    <div className="flex flex-row flex-wrap gap-x-8 justify-center gap-y-8 lg:flex-row lg:gap-x-[80px]">
                         <div className="">
                             <h3 className='font-bold text-xl mb-4'>Company</h3>
                             <ul className="flex flex-col gap-y-4">
