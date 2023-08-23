a8fc4d0c35aae1a99a9fb087724c1878329ad8e7
yannmarc <chamackstone@gmail.com>
2023-08-23 03:30:55 +0100

    the category card update

--- src/components/ui/CategoryCard.jsx
+++ src/components/ui/CategoryCard.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const CategoryCard = ({icon, heading, desc}) => {
   return (
     <Fragment>
-        <div className="p-8 lg:p-16 bg-white hover:shadow-xl transition-all ease-in-out rounded-xl lg:rounded-3xl md:max-w-[350px]">
+        <div className="p-8 lg:p-16 bg-gray-50 shadow-md lg:hover:shadow-xl transition-all ease-in-out rounded-xl lg:rounded-3xl md:max-w-[350px]">
             <div className="flex justify-center"><img src={icon} alt="" /></div>
             <div className="mt-[25px] lg:mt-[50px] text-center">
                 <h5 className="capitalize text-2xl font-normal">{heading}</h5>
