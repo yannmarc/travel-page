71402e79d0eddb78d646329489f68c54cdbb4997
yannmarc <chamackstone@gmail.com>
2023-08-23 03:31:48 +0100

    update max-width and text-align

--- src/components/ui/Headline.jsx
+++ src/components/ui/Headline.jsx
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react'
 
-const Headline = ({heading, subHeading, descText}) => {
+const Headline = ({heading, subHeading, descText, textAlign, maxWidth}) => {
   return (
     <Fragment>
-        <div className="max-w-[352px]">
+        <div className="max-w-[352px]" style={{textAlign: `${textAlign}`, marginInline: `${maxWidth}`}}>
             <span className="block uppercase text-[#5C8D96]">{subHeading}</span>
             <h2 className="font-medium text-[32px]">{subHeading}</h2>
             <p className="text-[#828282] mt-[40px]">{descText}</p>
