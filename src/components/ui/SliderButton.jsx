6afbccb0e01fa57b76d9053ec4f87a945482b386
yannmarc <chamackstone@gmail.com>
2023-08-23 03:32:21 +0100

    update of the button dimensions

--- src/components/ui/SliderButton.jsx
+++ src/components/ui/SliderButton.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const SliderButton = ({children, bgColor}) => {
   return (
     <Fragment>
-        <button className="p-6 shadow-lg rounded-full" style={{backgroundColor: `${bgColor}`}}>
+        <button className="shadow-lg rounded-full h-[50px] w-[50px] lg:h-[100px] lg:w-[100px] flex justify-center items-center" style={{backgroundColor: `${bgColor}`}}>
             {children}
         </button>
     </Fragment>
