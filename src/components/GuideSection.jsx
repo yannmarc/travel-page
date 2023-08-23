c11c84210803d76b0071a3c08efed5fef337ff25
yannmarc <chamackstone@gmail.com>
2023-08-23 03:27:39 +0100

    the guide section

--- src/components/GuideSection.jsx
+++ src/components/GuideSection.jsx
@@ -7,15 +7,15 @@ const GuideSection = () => {
   return (
     <Fragment>
             <section className='lg:relative py-[80px] lg:py-[140px]'>
-            <div className="main-container lg:max-w-[1100px] lg:mx-auto px-6 xl:px-0 flex flex-col lg:flex-row lg:flex lg:items-center gap-x-8">
+            <div className="main-container lg:max-w-[1100px] lg:mx-auto px-6 xl:px-0 flex flex-col lg:flex-row lg:flex xl:flex-col xl:items-start lg:items-center gap-x-8">
                 <div className="text">
                     <Headline 
                     subHeading={"SERVICES"}
                     heading={"Our top value categories for you "}
                     />
                 </div>
-                <div className="cards-section lg:w-[1050px] lg:absolute lg:right-[-152px]">
-                    <div className="flex lg:justify-center flex-col lg:flex-row">
+                <div className="cards-section lg:w-[1050px] lg:absolute xl:relative lg:right-[-152px] xl:right-0">
+                    <div className="flex lg:justify-center flex-col items-center gap-y-8 lg:gap-y-0 lg:flex-row lg:gap-x-6">
                         {
                             serviceContent.map((service) => (
                                 <CategoryCard key={service.id} heading={service.heading} icon={service.icon} desc={service.desc}/>
